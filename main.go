3a3fac1ff9be0ea65669e5b52acf061fc52305fd
Hknots <hknots99@gmail.com>
2024-03-16 23:11:42 +0100

    setup Router & Listen to port 8080

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"link-walker-go/controller"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	println("Hello world")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/task", controller.GetTasks).Methods("GET")
+	router.HandleFunc("/task", controller.PostTask).Methods("POST")
+
+	println("Started server at port: 8080")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		println("Failed to start server")
+	}
 }
+
+
