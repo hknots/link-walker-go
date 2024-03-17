6393f2306bc98c81298e666a37b0bb26b456a74b
Hknots <hknots99@gmail.com>
2024-03-17 11:14:02 +0100

    rename task

---
+++ cache/task.go
@@ -0,0 +1,34 @@
+package cache
+
+import (
+	"link-walker-go/types"
+	"sync"
+)
+
+var (
+	tasks = make(map[string]*types.Task)
+	mu    sync.RWMutex
+)
+
+func AddTask(task *types.Task) {
+	mu.Lock()
+	defer mu.Unlock()
+	tasks[task.ID] = task
+}
+
+func GetTask(id string) (*types.Task, bool) {
+	mu.RLock()
+	defer mu.RUnlock()
+	task, exists := tasks[id]
+	return task, exists
+}
+
+func GetAllTasks() []*types.Task {
+	mu.RLock()
+	defer mu.RUnlock()
+	allTasks := make([]*types.Task, 0, len(tasks))
+	for _, task := range tasks {
+		allTasks = append(allTasks, task)
+	}
+	return allTasks
+}
