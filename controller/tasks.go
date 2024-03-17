555c27d0d103f5e477ccb2ba72a53398d7f50d15
Hknots <hknots99@gmail.com>
2024-03-17 11:16:14 +0100

    create Task & Store in cache

--- controller/tasks.go
+++ controller/tasks.go
@@ -2,24 +2,38 @@ package controller
 
 import (
 	"encoding/json"
+	"github.com/google/uuid"
 	"link-walker-go/cache"
+	"link-walker-go/types"
 	"net/http"
 )
 
-func GetTasks(writer http.ResponseWriter, response *http.Request) {
-    tasks := cache.GetAllTasks()
-    tasksJSON, err := json.Marshal(tasks)
-    if err != nil {
-        http.Error(writer, "Failed to marshal tasks", http.StatusInternalServerError)
-        return
-    }
+func GetTasks(writer http.ResponseWriter, request *http.Request) {
+	tasks := cache.GetAllTasks()
+	tasksJSON, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(writer, "Failed to marshal tasks", http.StatusInternalServerError)
+		return
+	}
 
-    writer.Header().Set("Content-Type", "application/json")
-    writer.Write(tasksJSON)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(tasksJSON)
 }
 
-func PostTask(writer http.ResponseWriter, response *http.Request) {
+func PostTask(writer http.ResponseWriter, request *http.Request) {
+	token, err := GetAuthorizationToken(request)
+	if err != nil {
+		http.Error(writer, "Authorization must have Bearer <token>", http.StatusUnauthorized)
+		return
+	}
 
+	url, err := GetUrlFromBody(request)
+	if err != nil {
+		http.Error(writer, "url required in body", http.StatusBadRequest)
+		return
+	}
+
+	id := uuid.NewString()
+	task := types.NewTask(id, url, token)
+	cache.AddTask(task)
 }
-
-
